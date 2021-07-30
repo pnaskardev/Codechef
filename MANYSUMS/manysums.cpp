4270c8aac862e877dec05606085d82fdac7c2e5b
Priyanshu Naskar <71266237+pnaskardev@users.noreply.github.com>
2021-07-30 15:46:40 +0530

    Create manysums.cpp

---
+++ MANYSUMS/manysums.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+#define int long long int
+int32_t main(void)
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int l,r,sum=0;
+        cin>>l>>r;
+        int diff=r*2-l*2;
+        diff=diff+1;
+        cout<<diff<<endl;
+    }    
+}
