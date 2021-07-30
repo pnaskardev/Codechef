daf44b13dddbc59d797460d28ff2b760e86e0519
Priyanshu Naskar <71266237+pnaskardev@users.noreply.github.com>
2021-07-30 16:23:12 +0530

    Create HOOPS.cpp

---
+++ HOOPS/HOOPS.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#define int long long int
+using namespace std;
+int32_t main(void)
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        cout<<ceil((double)n/2)<<endl;
+    }
+}
