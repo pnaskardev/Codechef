e3b3ed398abc0fc89a32c06937146af9e4ad9b63
Priyanshu Naskar <71266237+pnaskardev@users.noreply.github.com>
2021-07-30 15:47:25 +0530

    Create sloop.cpp

---
+++ SLOOP/sloop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#define ll long long int 
+using namespace std;
+int main(void)
+{
+    ll test;
+    cin>>test;
+    while(test--)
+    {
+        ll m,s;
+        cin>>m>>s;
+        cout<<m/s<<endl;
+    }
+}
