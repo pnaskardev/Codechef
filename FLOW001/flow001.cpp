ef91b286968e0616478b1eb3d0b145273bf82825
Priyanshu Naskar <71266237+pnaskardev@users.noreply.github.com>
2021-07-30 15:45:55 +0530

    Create flow001.cpp

---
+++ FLOW001/flow001.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include<algorithm>
+using namespace std;
+int main(void)
+{
+    int test;
+    cin>>test;
+    while(test--)
+    {
+         long long a,b;
+        cin>>a>>b;
+        long long answer=a+b;
+        cout<<answer<<endl;
+    }
+}
