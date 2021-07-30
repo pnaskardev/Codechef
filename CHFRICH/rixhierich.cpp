d0a7a39cfdbf198f050f3f6cec8726bc50d508f4
Priyanshu Naskar <71266237+pnaskardev@users.noreply.github.com>
2021-07-30 15:44:26 +0530

    Create rixhierich.cpp

---
+++ CHFRICH/rixhierich.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#define int long long int
+using namespace std;
+void solve();
+int32_t main(void)
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+}
+void solve()
+{
+    int a,b,x;
+    cin>>a>>b>>x;
+    cout<<(b-a)/x<<endl;
+}
