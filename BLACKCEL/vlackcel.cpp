30688f7fc3661eda2c30a5aaed944053bb9b4beb
Priyanshu Naskar <71266237+pnaskardev@users.noreply.github.com>
2021-08-18 20:19:49 +0530

    Create vlackcel.cpp

---
+++ BLACKCEL/vlackcel.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#define int long long int
+using namespace std;
+void solve();
+int32_t main(void)
+{
+    solve();
+}
+void solve()
+{
+    int n;
+    cin>>n;
+    cout<<(n*n)/2<<endl;
+}
