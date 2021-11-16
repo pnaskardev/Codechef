6449d67d8b1c46d4baa3ed6cc24e50bc893b7416
Priyanshu Naskar <71266237+pnaskardev@users.noreply.github.com>
2021-11-16 17:52:30 +0530

    main

--- SIGNFLIP/prog.cpp
+++ SIGNFLIP/prog.cpp
@@ -14,5 +14,28 @@ int32_t main(void)
 }
 void solve()
 {
-       
+       int n,k;
+       cin>>n>>k;
+       vector<int>arr(n);
+       for(int i=0;i<n;i++)
+       {
+           cin>>arr[i];
+       }
+       sort(arr.begin(),arr.end());
+       int sum=0;
+       for(int i=0;i<k;i++)
+       {
+           if(arr[i]<0)
+           {
+               sum+=abs(arr[i]);
+           }
+       }
+       for(int i=0;i<n;i++)
+       {
+           if(arr[i]>0)
+           {
+               sum+=arr[i];
+           }
+       }
+       cout<<sum<<endl;
 }
