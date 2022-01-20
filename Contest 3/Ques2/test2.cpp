5c1a8fd5efcc122f19965d7196008bee9f268959
pnaskardev <pnaskardev@gmail.com>
2022-01-20 13:10:23 +0530

    main

---
+++ Contest 3/Ques2/test2.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#define int long long int
+using namespace std;
+const int maxim=1e5;
+int32_t main(void)
+{
+    srand(time(0));
+    fstream myIN;
+    myIN.open("in_0.in",ios::out);//write
+    fstream myOUT;
+    myOUT.open("out_0_second.out",ios::out);
+    int n;
+    cin>>n;
+    myIN<<n<<endl;
+    vector<int>a(n);
+    vector<int>o;
+    vector<int>e;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+        myIN<<a[i]<<" ";
+    }
+    myIN<<endl;
+    for(auto i:a)
+    {
+        if(i%2==0)
+        {
+            e.push_back(i);
+        }
+        else
+        {
+            o.push_back(i);
+        }
+    }
+    for(auto i:e)
+    {
+        cout<<i<<" ";
+        myOUT<<i<<" ";
+    }
+    for(auto i:o)
+    {
+        cout<<i<<" ";
+        myOUT<<i<<" ";
+    }
+    cout<<endl;
+    myOUT<<endl;
+}
